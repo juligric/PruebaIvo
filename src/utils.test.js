26639c894c37099c72069c55c274c1dfb8119829
agustina <49377944@est.ort.edu.ar>
2026-06-28 13:30:04 -0300

    agrego funcion de descuento con su test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,8 +1,14 @@
 import { describe, it, expect } from 'vitest';
-import { calcularTotal } from './utils.js';
+import { calcularTotal, aplicarDescuento } from './utils.js';
 
 describe('calcularTotal', () => {
   it('multiplica precio por cantidad', () => {
     expect(calcularTotal(100, 3)).toBe(300);
+  });
+});
+
+describe('aplicarDescuento', () => {
+  it('aplica un 20% de descuento correctamente', () => {
+    expect(aplicarDescuento(100, 20)).toBe(80);
   });
 });
