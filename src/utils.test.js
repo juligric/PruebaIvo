92629aa2db04246ce56aedd8ab0590ad1d443c49
agustina <49377944@est.ort.edu.ar>
2026-06-28 11:36:25 -0300

    arreglo el test

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,6 +3,6 @@ import { calcularTotal } from './utils.js';
 
 describe('calcularTotal', () => {
   it('multiplica precio por cantidad', () => {
-    expect(calcularTotal(100, 3)).toBe(999);
+    expect(calcularTotal(100, 3)).toBe(300);
   });
 });
